3f51468e18609556ff29d56d5923daaaa4a63231
Dmitry Kartesv <dmitry.kartsev@cnvrg.io>
2022-01-30 16:43:11 +0200

    wip

--- cmd/fractor/main.go
+++ cmd/fractor/main.go
@@ -54,7 +54,19 @@ var fractorStart = &cobra.Command{
 			log.Fatalf("Unable to get device count: %v", nvml.ErrorString(ret))
 		}
 
-		log.Info(count)
+		for i := 0; i < count; i++ {
+			device, ret := nvml.DeviceGetHandleByIndex(i)
+			if ret != nvml.SUCCESS {
+				log.Fatalf("Unable to get device at index %d: %v", i, nvml.ErrorString(ret))
+			}
+
+			uuid, ret := device.GetUUID()
+			if ret != nvml.SUCCESS {
+				log.Fatalf("Unable to get uuid of device at index %d: %v", i, nvml.ErrorString(ret))
+			}
+
+			log.Info("%v", uuid)
+		}
 
 		f := pkg.NewMetaFractorDevicePlugin()
 
